2b593c620f2f82d773f312003d9b289aa6b3ef62
Ibsham Chaudhary <progwithibi@gmail.com>
2023-11-03 15:51:37 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import favicon from "./assets/logo-fl-svgrepo-com.svg";
+import takequiz from "./assets/connected-svgrepo-com.svg";
+import profile from "./assets/istockphoto-825383494-612x612.jpg";
+import instagram from "./assets/instagram-round-svgrepo-com.svg";
+import facebook from "./assets/facebook.svg";
+import amazon from "./assets/amazon-round-svgrepo-com.svg";
+import share from "./assets/share-svgrepo-com(1).svg";
+const App = () => {
+  const socialLinks = [instagram, facebook, amazon];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="favicon">
+          <div className="favicon-main">
+            <img src={favicon} alt="logo" /> v.Blend
+          </div>
+          <div className="about">About</div>
+          <div className="e-gift">Send e-gift</div>
+        </div>
+        <div className="takequiz">
+          Take the Quiz
+          <img src={takequiz} alt="takequiz" />
+        </div>
+        <div className="social-container">
+          <div className="share">
+            <img src={share} alt="share" className="share-img" />
+          </div>
+          <div className="social-links">
+            {socialLinks.map((e) => {
+              return <img src={e} alt="social link" />;
+            })}
+          </div>
+          <img className="profile-pic" src={profile} alt="profile" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
